e26a0e60564edf6eba4c32ad43d47396ede70e66
Joshua Toguri <joshua.toguri@gmail.com>
2022-05-16 09:29:52 -0400

    implement and test conditional rendering of the number of spots left

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -9,10 +9,16 @@ export default function DayListItem(props) {
     'day-list__item--full': !props.spots
     });
 
+  const formatSpots = (spots) => {
+    if (spots > 1) return `${spots} spots remaining`;
+    if (spots === 1) return '1 spot remaining';
+    return 'no spots remaining';
+  }
+
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 }
